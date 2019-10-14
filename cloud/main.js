add962ff15e6f04f04044bd7154bd1fe4c341877
Eduardo Irías <eduardoirias@me.com>
2019-10-14 22:16:47 +0200

    Oups! 💩
    I removed something 🙈

--- cloud/main.js
+++ cloud/main.js
@@ -10,15 +10,6 @@
 // To see it working, you only need to call it through SDK or REST API.
 // Here is how you have to call it via REST API:
 
-/*
- * curl -X POST \
- * -H "X-Parse-Application-Id: 6UcvC2yYalNccIUzGa067RYWhCPeD4prd5pXsFWG" \
- * -H "X-Parse-REST-API-Key: dYJPLDwQIwNaUDM3yUnoDHbqOfEG4QPCLAWpWrPg" \
- * -H "Content-Type: application/json" \
- * -d "{}" \
- * https://parseapi.back4app.com/functions/hello
- */
-
 // If you have set a function in another cloud code file, called "test.js" (for example)
 // you need to refer it in your main.js, as you can see below:
 
